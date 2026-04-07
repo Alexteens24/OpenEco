d42cfcf9e5d313b562a93bdb9d63fc15131b63cd
Alexteens24 <Alexteens24@users.noreply.github.com>
2026-04-07 22:21:49 +0700

    build: bump PlaceholderAPI to 2.12.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("net.cfh.vault:VaultUnlocked:2.19.1")
-    compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("me.clip:placeholderapi:2.12.2")
     compileOnly("org.xerial:sqlite-jdbc:3.51.3.0")
     compileOnly("com.h2database:h2:2.4.240")
     implementation("org.bstats:bstats-bukkit:3.2.1")
@@ -53,7 +53,7 @@ dependencies {
     testImplementation("org.mockito:mockito-junit-jupiter:5.16.1")
     testImplementation("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     testImplementation("net.cfh.vault:VaultUnlocked:2.19.1")
-    testImplementation("me.clip:placeholderapi:2.11.6")
+    testImplementation("me.clip:placeholderapi:2.12.2")
     testRuntimeOnly("com.h2database:h2:2.4.240")
     testRuntimeOnly("org.xerial:sqlite-jdbc:3.51.3.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
